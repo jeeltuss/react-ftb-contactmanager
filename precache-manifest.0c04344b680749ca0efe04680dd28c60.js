8dd3f3e29549bc0ecd530ad0bca1ab196a0c663f
jeeltuss <jeeltuss@gmail.com>
2020-06-14 19:07:05 +0200

    Updates

---
+++ precache-manifest.0c04344b680749ca0efe04680dd28c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a1dd843fea9b24c2dc256d68de617b",
+    "url": "/react-ftb-contactmanager/index.html"
+  },
+  {
+    "revision": "b063800d2f20d3850570",
+    "url": "/react-ftb-contactmanager/static/css/main.ab4008d2.chunk.css"
+  },
+  {
+    "revision": "c996690243882db89d65",
+    "url": "/react-ftb-contactmanager/static/js/2.2d502c54.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-ftb-contactmanager/static/js/2.2d502c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b063800d2f20d3850570",
+    "url": "/react-ftb-contactmanager/static/js/main.9481dfd1.chunk.js"
+  },
+  {
+    "revision": "56da5d860c7ac6173cc4",
+    "url": "/react-ftb-contactmanager/static/js/runtime-main.9ae0d1c0.js"
+  }
+]);
